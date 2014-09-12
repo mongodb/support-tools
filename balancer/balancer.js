0e9a3d9e4b4d7798bcf13a54aa68a8556e03ba46
Jacob Ribnik <jribnik@gmail.com>
2014-09-12 12:53:42 -0400

    Added standard disclaimer and Apache license
    Added standard disclaimer and Apache 2.0 license

--- balancer/balancer.js
+++ balancer/balancer.js
@@ -5,6 +5,39 @@
  *
  * TODO Validate user input against config db
  *
+ *  DISCLAIMER
+ *
+ *  Please note: all tools/ scripts in this repo are released for use "AS
+ *  IS" without any warranties of any kind, including, but not limited to
+ *  their installation, use, or performance. We disclaim any and all
+ *  warranties, either express or implied, including but not limited to
+ *  any warranty of noninfringement, merchantability, and/ or fitness for
+ *  a particular purpose. We do not warrant that the technology will
+ *  meet your requirements, that the operation thereof will be
+ *  uninterrupted or error-free, or that any errors will be corrected.
+ *
+ *  Any use of these scripts and tools is at your own risk. There is no
+ *  guarantee that they have been through thorough testing in a
+ *  comparable environment and we are not responsible for any damage
+ *  or data loss incurred with their use.
+ *
+ *  You are responsible for reviewing and testing any scripts you run
+ *  thoroughly before use in any non-testing environment.
+ *
+ *
+ * LICENSE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 var SortSizeLarge = function(a, b){
