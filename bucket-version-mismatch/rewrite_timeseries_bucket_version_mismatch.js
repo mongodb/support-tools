cf0a0d773440a370f743c7a1fa435357c44ad03d
Stephanie Eristoff <stephanie.eristoff@mongodb.com>
2024-12-17 01:33:42 +0000

    Removed .getName()

--- bucket-version-mismatch/rewrite_timeseries_bucket_version_mismatch.js
+++ bucket-version-mismatch/rewrite_timeseries_bucket_version_mismatch.js
@@ -67,7 +67,7 @@ function runFixBucketVersionMismatchProcedure(collName) {
   // Range through all the bucketDocs and change the control version of the
   // bucket from 2 -> 3 if the data is not sorted or from 3 -> 2 if the data is
   // sorted.
-  const bucketsColl = db.getCollection('system.buckets.' + collName.getName());
+  const bucketsColl = db.getCollection('system.buckets.' + collName);
   var cursor = bucketsColl.find({});
 
   while (cursor.hasNext()) {
